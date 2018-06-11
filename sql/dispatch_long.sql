3dfa9b4ca405a95d544e9d2f0d33aee2a62c9ad5
hulklee01 <sungdl93@gmail.com>
2018-06-12 01:06:11 +0900

    add

--- sql/dispatch_long.sql
+++ sql/dispatch_long.sql
@@ -2,7 +2,7 @@ create table dispatch_long (
 num int not null identity(1,1),
 id varchar(30) not null,
 name varchar(30) not null,
-day varchar(35) not null,
+day int not null,
 start_t varchar(30) not null,
 end_t varchar(30) not null,
 car int not null,
